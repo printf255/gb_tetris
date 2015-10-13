99678e9a1adef054cb2cdf4b1c65251ecfdfe191
Lajos Ambrus <Dexter245@googlemail.com>
2015-10-13 17:39:47 +0200

    Implemented block stuff

--- src/block/block.h
+++ src/block/block.h
@@ -1,6 +1,23 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <gb/gb.h>
+
+extern const SHAPE_TYPE_I;
+extern const SHAPE_TYPE_J;
+extern const SHAPE_TYPE_L;
+extern const SHAPE_TYPE_O;
+extern const SHAPE_TYPE_S;
+extern const SHAPE_TYPE_T;
+extern const SHAPE_TYPE_Z;
+
+extern const BOOLEAN shape_grids[7][4][4][4];
+
+void rotate();
+void new_block(UINT8 type);
+
+BOOLEAN** get_grid();
+UINT8 get_tile();
 
 
 
